ec660e67c0b04a92533392448381487448e60d4d
Khong Minh Tri <khong.minh.tri@framgia.com>
2016-01-22 12:59:04 +0700

    51371 edit subject

--- app/controllers/admin/subjects_controller.rb
+++ app/controllers/admin/subjects_controller.rb
@@ -1,6 +1,7 @@
 class Admin::SubjectsController < ApplicationController
   layout "admin"
   before_action :logged_in_superuser
+  before_action :load_subject, only: [:edit, :update, :destroy]
 
   def index
     @subjects = Subject.paginate page: params[:page]
@@ -21,9 +22,31 @@ class Admin::SubjectsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @subject.update_attributes subject_params
+      flash[:success] = t "admin.subject.updated"
+      redirect_to admin_subjects_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @subject.destroy
+    flash[:success] = t "admin.subject.deleted"
+    redirect_to admin_subjects_path
+  end
+
   private
   def subject_params
     params.require(:subject).permit :name, :description,
-      tasks_attributes: [:id, :name, :description]
+      tasks_attributes: [:id, :name, :description, :_destroy]
+  end
+
+  def load_subject
+    @subject = Subject.find params[:id]
   end
 end
