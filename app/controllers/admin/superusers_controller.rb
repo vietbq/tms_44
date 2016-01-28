e4684f07d819abd2688e01b0165cc75f23d87b75
Khong Minh Tri <khong.minh.tri@framgia.com>
2016-01-28 11:53:13 +0700

    52960 show create edit delete admin

--- app/controllers/admin/superusers_controller.rb
+++ app/controllers/admin/superusers_controller.rb
@@ -1,17 +1,34 @@
 class Admin::SuperusersController < ApplicationController
   layout "admin"
-  before_action :logged_in_superuser, only: [:edit, :update]
+  before_action :logged_in_superuser
+  before_action :load_superuser, except: [:new, :create, :index]
   before_action :correct_superuser, only: [:edit, :update]
+
+  def new
+    @superuser = Superuser.new
+  end
+
+  def create
+    @superuser = Superuser.new superuser_params
+    if @superuser.save
+      flash[:success] = t "admin.admin.created_admin"
+      redirect_to admin_root_path
+    else
+      render :new
+    end
+  end
 
   def show
   end
 
+  def index
+    @superusers = Superuser.paginate page: params[:page]
+  end
+
   def edit
-    @superuser = Superuser.find params[:id]
   end
 
   def update
-    @superuser = Superuser.find params[:id]
     if @superuser.update_attributes superuser_params
       flash[:success] = t "user.update_success"
       redirect_to admin_superuser_path @superuser
@@ -20,14 +37,23 @@ class Admin::SuperusersController < ApplicationController
     end
   end
 
+  def destroy
+    @superuser.destroy
+    flash[:success] = t "admin.admin.deleted"
+    redirect_to admin_superusers_path
+  end
+
   private
   def superuser_params
     params.require(:superuser).permit :name, :email, :password,
       :password_confirmation
   end
 
-  def correct_superuser
+  def load_superuser
     @superuser = Superuser.find params[:id]
+  end
+
+  def correct_superuser
     redirect_to admin_superuser_path @superuser unless current_superuser? @superuser
   end
 end
