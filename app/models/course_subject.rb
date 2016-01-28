6cea38c95baef9a677b4fa9bdda39fdaf8559b20
Khong Minh Tri <khong.minh.tri@framgia.com>
2016-01-28 15:44:55 +0700

    51327 start finish course_subject

--- app/models/course_subject.rb
+++ app/models/course_subject.rb
@@ -3,4 +3,6 @@ class CourseSubject < ActiveRecord::Base
   belongs_to :subject
   has_many :course_subject_tasks, dependent: :destroy
   has_many :user_subjects, dependent: :destroy
+
+  enum status: [:not_start, :trainning, :finish]
 end
