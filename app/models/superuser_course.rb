a932f0d1ca106f0fee484b0a1d60573291b469ae
Khong Minh Tri <khong.minh.tri@framgia.com>
2016-01-29 12:31:06 +0700

    admin view course

--- app/models/superuser_course.rb
+++ app/models/superuser_course.rb
@@ -1,4 +1,7 @@
 class SuperuserCourse < ActiveRecord::Base
   belongs_to :superuser
   belongs_to :course
+
+  scope :get_current_courses,
+    -> superuser_id {where(superuser_id: superuser_id).order created_at: :asc}
 end
