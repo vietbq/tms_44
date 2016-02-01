250cee844cac31d198c25b98a1e6bc17f94a4564
buiquocviet <buiquocviet1993@gmail.com>
2016-02-01 08:26:26 +0700

    fix update status subject

--- app/helpers/user_courses_helper.rb
+++ app/helpers/user_courses_helper.rb
@@ -8,22 +8,28 @@ module UserCoursesHelper
   def finish_subject course_subject
     if course_subject.trainning?
       update_course_subject course_subject
-    end
-    user_subject = current_user.user_subjects.
-      find_by course_subject_id: course_subject.id
-    user_subject.update_attributes status: :finish
-    tasks = course_subject.course_subject_tasks
-    tasks.each do |task|
-      finish_task task
+      user_subject = current_user.user_subjects.
+        find_by course_subject_id: course_subject.id
+      user_subject.update_attributes status: :finish
+      tasks = course_subject.course_subject_tasks
+      tasks.each do |task|
+        finish_task task
+      end
+    else
+      flash[:danger] = t "subject.error_not_start"
     end
   end
 
   def finish_task course_subject_task
-    user_task = current_user.user_tasks.
-      find_by course_subject_task_id: course_subject_task
-    if user_task.present?
-      UserTask.create user_id: current_user.id,
-        course_subject_task_id: course_subject_task.id, status: :finish
+    if course_subject_task.course_subject.trainning?
+      user_task = current_user.user_tasks.
+        find_by course_subject_task_id: course_subject_task
+      if user_task.present?
+        current_user.user_tasks.create course_subject_task_id: course_subject_task,
+          status: :finish
+      end
+    else
+      flash[:danger] = t "subject.error_not_start"
     end
   end
 
